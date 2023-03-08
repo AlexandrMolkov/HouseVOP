c846dd4c1e4f11a7ba63394691643314ff0d33aa
AlexandrMolkov <alexandr.molkov95@gmail.com>
2023-03-08 12:39:24 +0300

    add aos

--- src/js/common/main.js
+++ src/js/common/main.js
@@ -17,7 +17,8 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
   
-  
+
+
   
   window.addEventListener('resize', () =>  {
     if(window.innerWidth > 1024) {
@@ -30,53 +31,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
   
 
-  ///////////////////// read more
-
-  const readmoreButtons = document.querySelectorAll('.read-more')
-  readmoreButtons.forEach((e) => {
-      e.addEventListener('click', (e) => {
-      const target = document.getElementById(e.target.dataset.target)
-      target.classList.add('show')})
-    })
+  AOS.init();
+  
     
 
 
 
 
-  /////////////////////  search
 
-  const searchBtn = document.querySelector('.search')
-  const headerForm = document.querySelector('.header__form-input-wrapper')
-  const inputExitBtn = document.querySelector('.header__form-input-exit')
-
- 
-
-  searchBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-
-    headerForm.classList.add('header__form-input_active')
-    blur.classList.add('blur_active')
-    document.querySelector('.burg').style.zIndex = 1
-
-    const hideInput = (e) => {
-        if(!e.target.classList.contains('header__form-input')) {
-            headerForm.classList.remove('header__form-input_active')
-            document.removeEventListener('click', hideInput)
-            document.removeEventListener('mousewheel', hideInput)
-            inputExitBtn.removeEventListener('click', hideInput)
-            blur.classList.remove('blur_active')
-            document.querySelector('.burg').style.zIndex = 100
-        }
-    }
-    
-    setTimeout(() => {
-        document.addEventListener('click', hideInput)
-    }, 100);
-
-    document.addEventListener('mousewheel', hideInput)
-    inputExitBtn.addEventListener('click', hideInput)
-
-  })
 })
 
 
